2b0a9f33426496bfb242de0d6ef554949223263a
Vladimir Boltin <boltinvlad@yandex.ru>
2022-09-22 17:00:50 +0300

    feat: make react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="header__left">
+          <img width={40} height={40} src="/image/logo_sneakers.png" />
+          <div>
+            <h3>REACT SNEAKERS</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="header__right">
+          <li>
+            <img width={18} height={18} src="image/basket.svg" />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="image/heart.svg" />
+          </li>
+          <li>
+            <img width={18} height={18} src="image/user.svg" />
+          </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+        ....
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
